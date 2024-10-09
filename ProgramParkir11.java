353ca5ccd1355bcba2302be0ab2450fbeecfe215
HafizRizqi <HafizRizqi>
2024-10-09 23:58:33 +0700

    TugasNo.2Jobsheet7

--- ProgramParkir11.java
+++ ProgramParkir11.java
@@ -4,27 +4,23 @@ public class ProgramParkir11 {
     public static void main(String[] args) {
         Scanner sc11 = new Scanner(System.in);
         int jenis, durasi, total = 0;
-        while (true) { 
+        do { 
             System.out.println("Masukkan jenis kendaraan (ketik '1' untuk Mobil,'2' untuk Motor, '0' untuk Keluar)");
         jenis = sc11.nextInt();
             if (jenis == 1 || jenis == 2) {
+                System.out.println("Masukkan durasi parkir (Per Jam): ");
+                durasi = sc11.nextInt();
+                if (durasi > 5) {
+                    total += 12500;
+                }
+                if (jenis == 1) {
+                    total += durasi * 3000;
+                }
+                if (jenis == 2) {
+                    total += durasi * 2000;
+                }
             }
-            if (jenis == 0) {
-                break;
-            }
-            System.out.println("Masukkan durasi parkir (Per Jam): ");
-            durasi = sc11.nextInt();
-            if (durasi > 5) {
-                total += 12500;
-            }
-            if (jenis == 1) {
-                total += durasi * 3000;
-            }
-            if (jenis == 2) {
-                total += durasi * 2000;
-            }
-
-            }      
+            } while (jenis != 0);
             System.out.println("Total pembayaran parkir: Rp" + total);
         } 
     }
